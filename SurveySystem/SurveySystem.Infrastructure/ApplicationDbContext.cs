e78f5a71a9be6a32ae2c46bc56bd50711e2589ca
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2025-01-09 16:58:10 +0100

    Update Infrastructure layer

--- SurveySystem/SurveySystem.Infrastructure/ApplicationDbContext.cs
+++ SurveySystem/SurveySystem.Infrastructure/ApplicationDbContext.cs
@@ -1,17 +1,71 @@
-﻿using SurveySystem.Domain.Abstractions;
-using Microsoft.Extensions.DependencyInjection;
+﻿using SurveySystem.Application.Exceptions;
+using SurveySystem.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace SurveySystem.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options) : base(options)
+    // Used for publishing domain event
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
+        : base(options)
     {
+        _publisher = publisher;
     }
 
-    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        throw new NotImplementedException();
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            // calls the base implementation of SaveChangesAsync to persist changes made in the context
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventsAsync();
+
+            // return value is the number of entries written to the db
+            return result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exception occurred.", ex);
+        }
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        // ChangeTracker is the property of DbContext
+        // keeps track of all entities in the context
+        var domainEvents = ChangeTracker
+            // Get all entries of type Entity
+            .Entries<Entity>()
+            // Transform EntityEntry objects from above to actual entity instances (e.g. Survey)
+            .Select(entry => entry.Entity)
+            // for each entity, get domain events associated with it
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                // Clear events, so they don't get published again
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
