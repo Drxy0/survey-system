9c202575bdef3e5c401ded22b3a8ec7230f59f23
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2025-01-16 21:02:16 +0100

    Add login page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserProfileComponent],
+  imports: [HeaderComponent, UserProfileComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
