9629f01fe6dde804c78ba5579d740becfd9bf8e7
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2025-01-17 22:45:03 +0100

    Update db connection string

--- SurveySystem/SurveySystem.Api/Program.cs
+++ SurveySystem/SurveySystem.Api/Program.cs
@@ -18,17 +18,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> b14d977 (Update db connection string)
-=======
->>>>>>> 906bf84 (fixed)
-=======
-
->>>>>>> b75042a (Rebase)
     app.ApplyMigrations();
 }
 
