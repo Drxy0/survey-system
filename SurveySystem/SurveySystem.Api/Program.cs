ca7b4eda8ca21d9895f950f55fe3258fe087ada6
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2025-01-19 17:27:24 +0100

    Feature/refactoring (#3)
    * Update db connection string
    
    * fixed
    
    ---------
    
    Co-authored-by: dualxyz <shikesenpu@gmail.com>

--- SurveySystem/SurveySystem.Api/Program.cs
+++ SurveySystem/SurveySystem.Api/Program.cs
@@ -13,29 +13,15 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> b14d977 (Update db connection string)
-=======
->>>>>>> 906bf84 (fixed)
-=======
-
->>>>>>> b75042a (Rebase)
     app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- SurveySystem/SurveySystem.Application/Abstractions/Data/ISqlConnectionFactory.cs
+++ SurveySystem/SurveySystem.Application/Abstractions/Data/ISqlConnectionFactory.cs
@@ -1,11 +1,11 @@
-﻿using System.Data;
-
-namespace SurveySystem.Application.Abstractions.Data;
-
-// This inteface is implemented in the infrastructure layer
-// Even still, the current, application layer uses this inteface
-// to call the implementation in the infrastructure layer by using dependency injection
-public interface ISqlConnectionFactory
-{
-    IDbConnection CreateConnection();
-}
+﻿// using System.Data;
+//
+// namespace SurveySystem.Application.Abstractions.Data;
+//
+// // This interface is implemented in the infrastructure layer
+// // Even still, the current, application layer uses this inteface
+// // to call the implementation in the infrastructure layer by using dependency injection
+// public interface ISqlConnectionFactory
+// {
+//     IDbConnection CreateConnection();
+// }
