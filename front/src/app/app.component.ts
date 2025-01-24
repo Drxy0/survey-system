3b3c2aaab6f15ba6a4a50a88a1dad24151b9c47b
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2025-01-24 22:24:25 +0100

    Add register page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { UserProfileComponent } from "./user-profile/user-profile.component";
-import { LoginComponent } from "./pages/login/login.component";
-import { SurveyQuestionComponent } from "./pages/create-survey/survey-question/survey-question.component";
-import { CreateSurveyComponent } from "./pages/create-survey/create-survey.component";
+import { HeaderComponent } from './header/header.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SurveyQuestionComponent } from './pages/create-survey/survey-question/survey-question.component';
+import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserProfileComponent, LoginComponent, SurveyQuestionComponent, CreateSurveyComponent],
+  imports: [
+    HeaderComponent,
+    UserProfileComponent,
+    LoginComponent,
+    SurveyQuestionComponent,
+    CreateSurveyComponent,
+    RegisterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'front';
---
+++ front/src/app/pages/register/register.component.ts
@@ -0,0 +1,88 @@
+import { Component, signal } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+function equalValues(inName1: string, inName2: string) {
+  return (control: AbstractControl) => {
+    const in1 = control.get(inName1)?.value;
+    const in2 = control.get(inName2)?.value;
+
+    if (in1 !== in2) {
+      return { valuesNotEqual: true };
+    }
+
+    return null;
+  };
+}
+
+function containsNonNumericSymbols(control: AbstractControl) {
+  if (control.value.match(/^\+[0-9]+$/)) {
+    return { doesNotContainInvalidCharacters: true };
+  }
+
+  return null;
+}
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  form = new FormGroup({
+    name: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    surname: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    address: new FormControl(),
+    city: new FormControl(),
+    country: new FormControl(),
+    phoneNumber: new FormControl('', {
+      validators: [containsNonNumericSymbols],
+    }),
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    passwords: new FormGroup(
+      {
+        password: new FormControl('', {
+          validators: [Validators.minLength(6), Validators.required],
+        }),
+        confirmPassword: new FormControl('', {
+          validators: [Validators.minLength(6), Validators.required],
+        }),
+      },
+      {
+        validators: [equalValues('password', 'confirmPassword')],
+      }
+    ),
+  });
+
+  onSubmit() {
+    console.log(this.form.controls.passwords.controls.confirmPassword);
+  }
+}
